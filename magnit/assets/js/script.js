4277f969015b20f5158a5834a482e601d9a7e85b
Filippov1996 <139255865+Filippov1996@users.noreply.github.com>
2024-07-12 00:57:54 +0300

    magnit

--- magnit/assets/js/script.js
+++ magnit/assets/js/script.js
@@ -1,10 +1,24 @@
  $(document).ready(function(){
 	 
 	 var sliderNewes = new Swiper(".news__slider", {
-      slidesPerView: 3,
-      spaceBetween: 20,
-		loop:true,
-      freeMode: true
+       spaceBetween: 20,
+	   loop:true,
+       freeMode: true,
+	   breakpoints: {
+		     0: {
+				  slidesPerView: 1,
+				  spaceBetween: 20,
+				},
+				570: {
+				  slidesPerView: 2,
+				  spaceBetween: 20,
+				},
+				865: {
+				  slidesPerView: 3,
+				  spaceBetween: 40,
+				}
+				
+	   }
     });
 	 
 	 
