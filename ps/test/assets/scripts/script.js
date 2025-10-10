eddfbfe837a4ebcd3a8f1ebe0135c237caaaf112
Filippov1996 <139255865+Filippov1996@users.noreply.github.com>
2025-10-10 23:21:55 +0300

    sdasd

---
+++ ps/test/assets/scripts/script.js
@@ -0,0 +1,50 @@
+$(document).ready(function() { 
+    var swiper = new Swiper(".swiperItems", {
+       slidesPerView: "auto",
+      spaceBetween: 10,  
+       navigation: {
+        nextEl: ".btn-next",
+        prevEl: ".btn-prev"
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      }
+    });
+
+    $(".js-burger").on('click',function(){
+        $('.banner__nav-mob-wl').slideToggle(400);
+        
+        $(".js-burger .item-top").toggleClass("move");
+        $(".js-burger .item-bot").toggleClass("move");
+        
+        
+        
+    });
+    
+    
+
+    $(".js-data .point").each(function(i){
+                $(this).on('click',function(){
+                    $(".js-data .point").removeClass("active");
+                    $(this).addClass("active");
+                
+                
+                    swiper.slideTo(i);
+            });    
+    });
+    
+    
+    
+    swiper.on('slideChange', function () {
+    //console.info('*** mySwiper.realIndex', mySwiper.realIndex);
+         $(".js-data .point").removeClass("active");
+         $(".js-data .point").eq(swiper.realIndex).addClass("active");
+    });
+    
+    
+    
+    
+    
+    
+});
